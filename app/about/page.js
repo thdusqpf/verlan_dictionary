461e5cad0fc47f752f78fce16dfd92f765e33301
sseoni <82037889+sseoni@users.noreply.github.com>
2024-03-25 17:03:40 +0900

    page.js

--- app/about/page.js
+++ app/about/page.js
@@ -1,3 +1,22 @@
 export default function About() {
-  return <p>This is About Page.</p>;
+  return <p><div className="v27_76">
+            <div className="v27_77">
+                <span className="v27_78">[verlan]</span>
+                <div className="v27_79">C'est une forme d’argot que formé en inversant les syllabes d’un mot</span>
+                    <div className="v27_81"> 단어를 음절 단위로 나누어 순서를 바꾸는 프랑스의 은어를 의미합니다.
+보통 두 음절의 단어로부터 파생되며 두 음절 이상이 verlan으로 사용되는 경우는 음운축약 등의 이유로 실제로는 두 음절로 발음되는 경우입니다.
+12세기 프랑스 음유시인 Béroul의 소설에서도 찾아볼 수 있는 프랑스인들의 오랜 언어습관입니다.
+이후 여러 문학에서도 verlan을 확인할 수 있으나 그 빈도가 잦지 않았고 최근 젊은 세대에 널리 퍼지게 된 것은 1990년대 힙합이 유행하면서였습니다.
+90년대에는 이민2세들 사이에서 폭력적인 언어로 유행하였으나 현재는 그 의미가 퇴색되어 단순히 유행어의 역할을 하고있습니다.
+                    </div>
+                </div>
+                <div className="v27_83">
+                    <div className="v27_84">à l'envers[lɑ̃vɛʁ] : 반대로
+[lɑ̃ / vɛʁ] --> [vɛʁ / lɑ̃]
+                    </div>
+                </div>
+            </div>
+            <div className="v27_86"></div>
+        </div>
+  </p>;
 }
