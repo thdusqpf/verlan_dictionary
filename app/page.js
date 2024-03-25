bfba7b6d69287a48057c84a378eeaa5bdbccf62f
sseoni <82037889+sseoni@users.noreply.github.com>
2024-03-25 17:15:57 +0900

    page.js

--- app/page.js
+++ app/page.js
@@ -17,9 +17,23 @@ function WordCloud() {
     <Row className="wcloud-bg">
       <Col>
         <h2 style={{ marginTop: "60px" }}>Word Cloud</h2>
-        <Button variant="dark" className="black-btn">
-          Save
-        </Button>
+          <div class="ligne">
+              <div class="intro primaire">
+                  <h2>Verlan les plus fr&eacute;quents</h2>
+                  <p>
+                      <a title="verlan Cimer" href="/conjugaison/verbe/acheter.html" class="t18">cimer</a>
+                      <a title="verlan chelou" href="/conjugaison/verbe/acheter.html" class="t18">chelou</a>
+                      <a title="verlan ouf" href="/conjugaison/verbe/acheter.html" class="t18">ouf</a>
+                      <a title="verlan zarbi" href="/conjugaison/verbe/acheter.html" class="t18">zarbi</a>
+                      <a title="verlan keufs" href="/conjugaison/verbe/acheter.html" class="t18">keufs</a>
+                      <a title="verlan meuf" href="/conjugaison/verbe/acheter.html" class="t18">meuf</a>
+                      <a title="verlan keum" href="/conjugaison/verbe/acheter.html" class="t18">keum</a>
+                      <a title="verlan cefran" href="/conjugaison/verbe/acheter.html" class="t18">cefran</a>
+                      <a title="verlan teuf" href="/conjugaison/verbe/acheter.html" class="t18">teuf</a>
+                      <a title="verlan cheum" href="/conjugaison/verbe/acheter.html" class="t18">cheum</a>
+                  </p>
+              </div>
+          </div>
       </Col>
       <Col style={{ height: "100%" }}>
         <div className="search-card">
