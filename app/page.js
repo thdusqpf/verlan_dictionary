a733923b68d8f11fda7d942f7ff3703f10684353
sseoni <82037889+sseoni@users.noreply.github.com>
2024-03-25 20:45:43 +0900

    page.js

--- app/page.js
+++ app/page.js
@@ -25,16 +25,16 @@ function WordCloud() {
                
                 <h2>Verlan les plus fr&eacute;quents</h2>
                   <p>
-                      <a title="verlan Cimer" href="/conjugaison/verbe/acheter.html" class="t18">cimer</a>
-                      <a title="verlan chelou" href="/conjugaison/verbe/acheter.html" class="t18">chelou</a>
-                      <a title="verlan ouf" href="/conjugaison/verbe/acheter.html" class="t18">ouf</a>
-                      <a title="verlan zarbi" href="/conjugaison/verbe/acheter.html" class="t18">zarbi</a>
-                      <a title="verlan keufs" href="/conjugaison/verbe/acheter.html" class="t18">keufs</a>
-                      <a title="verlan meuf" href="/conjugaison/verbe/acheter.html" class="t18">meuf</a>
-                      <a title="verlan keum" href="/conjugaison/verbe/acheter.html" class="t18">keum</a>
-                      <a title="verlan cefran" href="/conjugaison/verbe/acheter.html" class="t18">cefran</a>
-                      <a title="verlan teuf" href="/conjugaison/verbe/acheter.html" class="t18">teuf</a>
-                      <a title="verlan cheum" href="/conjugaison/verbe/acheter.html" class="t18">cheum</a>
+                      <a title="verlan Cimer" href="/conjugaison/verbe/acheter.html" class="wordcloud1">cimer</a>
+                      <a title="verlan chelou" href="/conjugaison/verbe/acheter.html" class="wordcloud2">chelou</a>
+                      <a title="verlan ouf" href="/conjugaison/verbe/acheter.html" class="wordcloud3">ouf</a>
+                      <a title="verlan zarbi" href="/conjugaison/verbe/acheter.html" class="wordcloud4">zarbi</a>
+                      <a title="verlan keufs" href="/conjugaison/verbe/acheter.html" class="wordcloud5">keufs</a>
+                      <a title="verlan meuf" href="/conjugaison/verbe/acheter.html" class="wordcloud6">meuf</a>
+                      <a title="verlan keum" href="/conjugaison/verbe/acheter.html" class="wordcloud7">keum</a>
+                      <a title="verlan cefran" href="/conjugaison/verbe/acheter.html" class="wordcloud8">cefran</a>
+                      <a title="verlan teuf" href="/conjugaison/verbe/acheter.html" class="wordcloud9">teuf</a>
+                      <a title="verlan cheum" href="/conjugaison/verbe/acheter.html" class="wordcloud10">cheum</a>
                   </p>
               </div>
           </div>
