2b58708e4bcccff328ab690d04b993aa7c8329cb
thdusqpf <61140818+thdusqpf@users.noreply.github.com>
2024-05-25 10:09:48 +0900

    Update search.js

--- pages/api/search.js
+++ pages/api/search.js
@@ -3,21 +3,10 @@ import { connectDB } from "@/util/database";
 export default async function handler(request, response) {
   if (request.method == "POST") {
     if (request.body.keyword == "") {
-      return response.status(500).json("단어를 입력해주세요");
+      return response.status(302).redirect("/search");
     }
     console.log(request.body.keyword);
     console.log("POST requested");
-    const db = (await connectDB).db("forum");
-    let result = await db
-      .collection("dictionary")
-      .findOne({ Verlan: request.body.keyword });
-
-    await db
-      .collection("dictionary")
-      .updateOne(
-        { Verlan: result.Verlan },
-        { $set: { Count: result.Count + 1 } }
-      );
     response.status(302).redirect(`/search?keyword=${request.body.keyword}`);
   }
 }
