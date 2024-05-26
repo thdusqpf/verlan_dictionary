005a64537e3d04fe3d7d496de324a4abf0a8c4ab
thdusqpf <61140818+thdusqpf@users.noreply.github.com>
2024-05-27 02:20:15 +0900

    악센트

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,12 +1,15 @@
-import { connectDB } from "@/util/database";
-
 export default async function handler(request, response) {
   if (request.method == "POST") {
     if (request.body.keyword == "") {
       return response.status(302).redirect("/search");
     }
-    console.log(request.body.keyword);
+
+    const remove_keyword = removeAccents(request.body.keyword);
     console.log("POST requested");
-    response.status(302).redirect(`/search?keyword=${request.body.keyword}`);
+    response.status(302).redirect(`/search?keyword=${remove_keyword}`);
   }
+}
+
+function removeAccents(str) {
+  return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
